6fdae7cc5bf2257735b10662eaf477e497ccc812
shenhuaxin <shenhuaxin@extracme.com>
2021-03-29 17:43:17 +0800

    提交代码

--- rpc-consumer/src/main/java/org/consumer/RpcConsumerPostProcessor.java
+++ rpc-consumer/src/main/java/org/consumer/RpcConsumerPostProcessor.java
@@ -1,11 +1,61 @@
 package org.consumer;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.consumer.annotation.RpcReference;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
 
-public class RpcConsumerPostProcessor {
+import java.lang.reflect.Field;
 
+@Slf4j
+@Component
+public class RpcConsumerPostProcessor implements ApplicationContextAware, BeanClassLoaderAware, BeanFactoryPostProcessor {
 
+    private ApplicationContext context;
 
+    private ClassLoader classLoader;
 
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context = context;
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
+            String beanClassName = beanDefinition.getBeanClassName();
+            if (beanClassName != null) {
+                Class<?> clazz = ClassUtils.resolveClassName(beanClassName, classLoader);
+                ReflectionUtils.doWithFields(clazz, this::parseRpcReference);
+            }
+        }
+    }
+
+    private void parseRpcReference(Field field) {
+        RpcReference annotation = AnnotationUtils.getAnnotation(field, RpcReference.class);
+        if (annotation != null) {
+            BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(RpcReferenceBean.class);
+            builder.setInitMethodName("init");
+            builder.addPropertyValue("interfaceClass", field.getType());
+
+        }
+    }
 }
